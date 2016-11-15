8befeb9328fee5e9fc357ea573df35cd3c3e5550
Matheus Leite <matheus.leite27@gmail.com>
2016-11-15 00:55:25 -0200

    update gitignore and push to remote

---
+++ Pods/Target Support Files/Pods-GateUp/Pods-GateUp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GateUp : NSObject
+@end
+@implementation PodsDummy_Pods_GateUp
+@end
---
+++ Pods/Target Support Files/Pods-GateUp/Pods-GateUp-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GateUpVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GateUpVersionString[];
+
